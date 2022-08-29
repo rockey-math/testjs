b15a3c1976797cc2d77810eaeeeea54e9e0e4e68
rockeymath <112349400+rockeymath@users.noreply.github.com>
2022-08-28 21:20:26 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -40,7 +40,7 @@ function connect() {
 
 function drawShape() {
 		values[valuePointer++ & 255] = y;
-	        values[valuePointer++ & 254] = x;
+	        // values[valuePointer++ & 254] = x;
 		context.beginPath();
 		context.strokeStyle = "rgba(0,0,255,1)";
 		context.moveTo( 0.5, y + 0.5);
