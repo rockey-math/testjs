22b3fbf01b596ea8bb5221c24bf5c68b76340c79
rockeymath <112349400+rockeymath@users.noreply.github.com>
2022-08-28 20:02:15 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -42,7 +42,7 @@ function drawShape() {
 		context.beginPath();
 		context.strokeStyle = "rgba(0,0,255,1)";
 		context.moveTo(0.0, 0.0);
-	        context.lineTo(10.0, y + 10.0);
+	        context.lineTo(x, y);
 		// for (var i = 1; i < 256; ++i) {
 		// 	context.lineTo(256 + i + 0.5, values[(valuePointer - i) & 255] + 0.5);
 		// }
